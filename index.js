0c4be930d9bd65071e73cdd216503492631ed1b5
toddys <viniciusgabriez07@gmail.com>
2021-11-11 09:58:54 -0300

    s

---
+++ index.js
@@ -0,0 +1,28 @@
+/*alert("Toddys é lindo");*/
+var a = 10;
+console.log("Hello, World!" + a);
+
+for(var i=150;i<301;i++){
+    console.log(i);
+}
+
+var soma = 0;
+for(var j=1;j<=1000;j++){
+    soma += j;
+    console.log(" " + soma);
+}
+
+for(var k=1;k<=100;k++){
+    if(k%3 === 0){
+        console.log(k +" é multiplo de tres");
+    }
+}
+
+var fat = 1;
+for(var l=1;l<=10;l++){
+    fat *= l;
+    console.log(fat + " ");
+}
+
+
+
