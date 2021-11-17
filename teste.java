b7886c4f4b47e390de41500a13c3e21037ad61f1
toddys <viniciusgabriez07@gmail.com>
2021-11-17 13:45:57 -0300

    ~s

---
+++ teste.java
@@ -0,0 +1,13 @@
+public class Pokemon extends Animal{
+    private int HPTotal;
+    private int HPAtual;
+    private int iniciativa;
+    private String tipo1;
+    private String tipo2;
+    private int ataque;
+    
+    int[] ataque = new int[4];
+    
+
+    
+}
