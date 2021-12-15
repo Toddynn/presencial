0758f4e35d8786d100b15c6c8bae90dd8f228180
Toddynn <viniciusgabriez07@gmail.com>
2021-12-15 10:09:38 -0300

    as

--- exerciciosaula/exercicio4.js
+++ exerciciosaula/exercicio4.js
@@ -1,18 +1,27 @@
 document.querySelector("#adicionar").addEventListener("click", adicionar);
 document.querySelector("tbody").addEventListener("mouseover", mudarCor);
 
+if(localStorage.length > 0){
+    let nome = localStorage.getItem('nome');
+    let idade = localStorage.getItem('idade');
+
+}
 function mudarCor(event){
     var evento = event.target.parentNode;
     console.log(evento);
 
     evento.classList.toggle("cor");
 }
+
 function adicionar(event){
     event.preventDefault();
 
     let form = document.querySelector("#form-adicionar");
     let nome = form.nome.value;
     let idade = form.idade.value;
+
+    localStorage.setItem('nome', nome);
+    localStorage.setItem('idade', idade);
 
     let linha = document.createElement("tr");
     let celulaNome = document.createElement('td');
