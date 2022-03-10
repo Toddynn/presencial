f04d7e8d2abcbd05d0c21a4c06c55bc4804edf6f
Toddynn <viniciusgabriez07@gmail.com>
2022-03-10 09:57:02 -0300

    s

--- exerciciosaula2(conteudonovo)/exercicio2.js
+++ exerciciosaula2(conteudonovo)/exercicio2.js
@@ -1,6 +1,17 @@
 
-var num = [4, 2, 6, 8, 10];
+let num = [4, 2, 6, 8, 10];
 
-function Calc(){
-    
+function CalcForEach(valor){
+    let resultado = math.pow(valor, 2);
+    console.log("indice" + indice,"valor" + valor,"resultado" + resultado);
 }
+
+function CalcMap(valor){
+    let resultado = valor * valor;
+    return resultado;
+}
+
+numeros.ForEach(CalcForEach);
+
+let resultado = num.map(CalcMap);
+console.log(resultado);
