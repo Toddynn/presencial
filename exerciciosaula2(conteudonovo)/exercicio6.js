37d541992333e4f9a84928b463e32eb303bc62d2
Toddynn <viniciusgabriez07@gmail.com>
2022-06-20 12:02:17 -0300

    s

--- exerciciosaula2(conteudonovo)/exercicio6.js
+++ exerciciosaula2(conteudonovo)/exercicio6.js
@@ -1,42 +1,36 @@
 document.querySelector("#adicionar").addEventListener("click", adicionar);
+document.querySelector("#somar").addEventListener("click", somar);
+document.querySelector("#filtrar").addEventListener("click", filtrar);
 
-iniciarTabela(nomes, idades, patrimonios);
+var registros = [];
+var registrosTemporarios = [];
+var filtroIdade;
+
+function adicionarRegistro(nome, idade, patrimonio){
+    registros.push({nome: nome, idade: idade, patrimonio: patrimonio});
+}
+
+function adicionarRegistroTemporario(nome, idade, patrimonio){
+    registros.push({nome: nome, idade: idade, patrimonio: patrimonio});
+}
 
 function adicionar(event){
     event.preventDefault();
 
-    var form = document.querySelector("#form-adiciona");
-    nome = form.nome.value;
-    idade = form.idade.value;
-    patrimonio = form.patrimonio.value;
+    let form = document.querySelector("#formulario");
+    let nome = form.nome.value;
+    let idade = form.idade.value;
+    let patrimonio = form.patrimonio.value;
 
-    nomes = JSON.parse(localStorage.getItem("nomes"));
-    idades = JSON.parse(localStorage.getItem("idades"));
-    patrimonios = JSON.parse(localStorage.getItem("patrimonios"));
-
-    nomes.push(nome);
-    idades.push(idade);
-    patrimonios.push(patrimonios);
-
-    localStorage.setItem("nomes", JSON.stringify(nomes));
-    localStorage.setItem("idades", JSON.stringify(idades));
-    localStorage.setItem("patrimonios", JSON.stringify(patrimonios));
-
+    adicionarRegistro(nome, idade, patrimonio);
     criarLinha(nome, idade, patrimonio);
-    form.reset();
-}
-
-function iniciarTabela(nomes, idades, patrimonios){
-    for(i = 0; i < nomes.length; i++){
-        criarLinha(nomes[i], idades[i], patrimonios[i]);
-    }
 }
 
 function criarLinha(formNome, formIdade, formPatrimonio){
-    var linha = document.createElement('tr');
-    var nome = document.createElement('td');
-    var idade = document.createElement('td');
-    var patrimonio = document.createElement('td');
+    let linha = document.createElement('tr');
+    let nome = document.createElement('td');
+    let idade = document.createElement('td');
+    let patrimonio = document.createElement('td');
 
     nome.textContent = formNome;
     idade.textContent = formIdade;
@@ -45,6 +39,39 @@ function criarLinha(formNome, formIdade, formPatrimonio){
     linha.appendChild(nome);
     linha.appendChild(idade);
     linha.appendChild(patrimonio);
-    document.querySelector("tbody").appendChild(linha);
+    document.querySelector('tbody').appendChild(linha);
+
+}
+
+function filtrarIdade(registro){
+    return registro.idade >= filtroIdade;
+}
+
+function filtrar(event){
+    event.preventDefault();
+
+    filtroIdade = parseInt(document.querySelector('#filtroIdade').value);
+
+    registrosTemporarios = registros.filter(filtrarIdade);
+
+    let tabela = document.querySelector('tbody');
+    tabela.innerHTML = '';
+    for(let registro of registrosTemporarios){
+        criarLinha(registro.nome, registro.idade, registro.patrimonio);
+    }
+}
+
+function somar(event){
+    event.preventDefault();
+
+    let soma = 0;
+
+    if(registrosTemporarios.length === 0){
+        registrosTemporarios = registros;
+    }
+    for(let registro of registrosTemporarios){
+        soma += parseInt(registro.patrimonio);
+    }
+    document.querySelector("#somatoria").value = soma;
 
 }
